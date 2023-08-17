45ebbde7cc54570bffd1f5c4a00f0705c26f9f0d
kbz85 <248997917@qq.com>
2023-08-17 11:04:34 +0800

    滑块验证码

--- src/pages/captcha/index.tsx
+++ src/pages/captcha/index.tsx
@@ -1,39 +1,77 @@
 // 是否刷新
 // 图片url
 // 小卡片位置
-import { ReactNode } from 'react';
+import { View } from "@tarojs/components";
+import { ReactNode } from "react";
 
 interface IvertifyProp {
   /**
    * @description: canvas宽度
    * @default 320
    */
-  width: number
+  width: number;
   /**
-  * @description: canvas高度
-  * @default 160
-  */
-  height: number
+   * @description: canvas高度
+   * @default 160
+   */
+  height: number;
   /**
-  * @description:滑块边长
-  * @default 42
-  */
-  sliderLen: number
+   * @description:滑块边长
+   * @default 42
+   */
+  sliderLen: number;
   /**
-  * @description: 滑块半径
-  * @default 9
-  */
-  sliderRidius: number
+   * @description: 滑块半径
+   * @default 9
+   */
+  sliderRidius: number;
   /**
-  * @description: 是否可见
-  * @default true
-  */
-  visible: boolean
+   * @description: 是否可见
+   * @default true
+   */
+  visible: boolean;
   /**
-  * @description: 滑块文本
-  * @default 向右滑动填充平图
-  */
-  text:string | ReactNode
+   * @description: 滑块文本
+   * @default 向右滑动填充平图
+   */
+  text: string | ReactNode;
 }
 
-export default
+export default ({
+  width = 320,
+  height = 160,
+  l = 42,
+  r = 9,
+  imgUrl,
+  text,
+  refreshIcon = "http://yourimgsite/icon.png",
+  visible = true,
+  onSuccess,
+  onFail,
+  onRefresh,
+}: IvertifyProp) => {
+  function handleRefresh() {}
+  const textTip = ''
+  const sliderClass = ''
+  return (
+    <View className='vertifyWrap'>
+      <View className='canvasArea'>
+        <canvas width={width} height={height}></canvas>
+        <canvas className='block' width={width} height={height}></canvas>
+      </View>
+      <View className={sliderClass}>
+        <View className='sliderMask'>
+          <View className='slider'>
+            <View className='sliderIcon'>&rarr;</View>
+          </View>
+        </View>
+        <View className='sliderText'>{textTip}</View>
+      </View>
+      <View className='refreshIcon' onClick={handleRefresh}></View>
+      <View className='loadingContainer'>
+        <View className='loadingIcon'></View>
+        <span>加载中...</span>
+      </View>
+    </View>
+  );
+};
--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,3 +1,3 @@
 export default definePageConfig({
-  navigationBarTitleText: '首页'
+  navigationBarTitleText: '滑块验证码'
 })
