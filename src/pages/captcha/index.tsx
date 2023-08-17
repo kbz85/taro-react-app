2a6f7d01c8e1405fef294f587a31bdad7f1ccccf
kbz85 <248997917@qq.com>
2023-08-17 17:09:17 +0800

    1

--- src/pages/captcha/index.tsx
+++ src/pages/captcha/index.tsx
@@ -2,8 +2,8 @@
 // 图片url
 // 小卡片位置
 import { View } from "@tarojs/components";
-import { ReactNode, useState } from "react";
-import './index.scss';
+import { ReactNode, useEffect, useState } from "react";
+import "./index.scss";
 
 interface IvertifyProp {
   /**
@@ -38,6 +38,45 @@ interface IvertifyProp {
   text: string | ReactNode;
 }
 
+function drawPuzzlePiece() {
+  const imgDom = document.getElementById("image-area") as HTMLDivElement;
+  const canvas = document.createElement("canvas") as HTMLCanvasElement;
+  canvas.classList.add("puzzle-piece");
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  // ctx.fillStyle = "#000";
+  // ctx.fillRect(10, 10, 150, 100);
+  drawPath(ctx);
+
+  imgDom.appendChild(canvas);
+}
+function drawPath(ctx: CanvasRenderingContext2D) {
+  ctx.beginPath();
+  const PI = Math.PI;
+  const r = 5;
+  const l = 20;
+  const x = 100;
+  const y = 100;
+  console.log(r, l, x, y);
+
+  ctx.moveTo(x, y);
+  ctx.arc(x + l / 2, y - r + 2, r, 0.72 * PI, 2.26 * PI);
+  ctx.lineTo(x + l, y);
+  ctx.arc(x + l + r - 2, y + l / 2, r, 1.21 * PI, 2.78 * PI);
+  ctx.lineTo(x + l, y + l);
+  ctx.lineTo(x, y + l);
+  // anticlockwise为一个布尔值。为true时，是逆时针方向，否则顺时针方向
+  ctx.arc(x + r - 2, y + l / 2, r + 0.4, 2.76 * PI, 1.24 * PI, true);
+  ctx.lineTo(x, y);
+  ctx.fillStyle = "rgba(0, 0, 255, 0.8)";
+  ctx.strokeStyle = "rgba(0, 0, 255, 0.8)";
+  // ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+  // ctx.strokeStyle = "rgba(255, 255, 255, 0.8)";
+  ctx.stroke();
+  ctx.globalCompositeOperation = 'destination-over'
+    // 判断是填充还是裁切, 裁切主要用于生成图案滑块
+    // operation === 'fill'? ctx.fill() : ctx.clip()
+}
+
 export default ({
   width = 320,
   height = 160,
@@ -52,25 +91,29 @@ export default ({
   onRefresh,
 }: IvertifyProp) => {
   function handleRefresh() {}
-  const [sliderClass, setSliderClass] = useState('slider-container');
-  const [textTip, setTextTip] = useState('123');
+  const [sliderClass, setSliderClass] = useState("slider-container");
+  const [textTip, setTextTip] = useState("123");
+  useEffect(() => {
+    drawPuzzlePiece();
+  });
+
   return (
-    <View className='vertify-wrap'>
-      <View className='canvas-area'>
-        <canvas className='canvas'></canvas>
-        <canvas className='canvas block'></canvas>
+    <View className="vertify-wrap">
+      <View className="canvas-area" id="image-area">
+        <canvas className="canvas-1"></canvas>
+        <canvas className="block canvas-2"></canvas>
       </View>
       <View className={sliderClass}>
-        <View className='slider-mask'>
-          <View className='slider'>
-            <View className='sliderIcon'>&rarr;</View>
+        <View className="slider-mask">
+          <View className="slider">
+            <View className="sliderIcon">&rarr;</View>
           </View>
         </View>
-        <View className='sliderText'>{textTip}</View>
+        <View className="sliderText">{textTip}</View>
       </View>
-      <View className='refreshIcon' onClick={handleRefresh}></View>
-      <View className='loading-container'>
-        <View className='loadingIcon'></View>
+      <View className="refreshIcon" onClick={handleRefresh}></View>
+      <View className="loading-container">
+        <View className="loadingIcon"></View>
         <span>加载中...</span>
       </View>
     </View>
